00adefcd62b560a0bff6172d78a53630a017b5dd
wwb <wangweibin@newland.com.cn>
2023-06-25 23:45:36 +0800

    资源本地化

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+
+import React from 'react'
+import Spinner from './Spinner'
 import './App.css';
 
-function App() {
+function RepeatButton(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      aria-label='Play again.'
+      id='repeatButton'
+      onClick={props.onClick}>
+    </button>
   );
 }
 
-export default App;
+function WinningSound() {
+  return (
+    <audio autoplay="autoplay" className="player" preload="false">
+      <source src="https://andyhoffman.codes/random-assets/img/slots/winning_slot.wav" />
+    </audio>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      winner: null
+    }
+    this.finishHandler = this.finishHandler.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ winner: null });
+    this.emptyArray();
+    this._child1.forceUpdateHandler();
+    this._child2.forceUpdateHandler();
+    this._child3.forceUpdateHandler();
+  }
+
+  static loser = [
+    'Not quite',
+    'Stop gambling',
+    'Hey, you lost!',
+    'Ouch! I felt that',
+    'Don\'t beat yourself up',
+    'There goes the college fund',
+    'I have a cat. You have a loss',
+    'You\'re awesome at losing',
+    'Coding is hard',
+    'Don\'t hate the coder'
+  ];
+
+  static matches = [];
+
+  finishHandler(value) {
+    App.matches.push(value);
+
+    if (App.matches.length === 3) {
+      const { winner } = this.state;
+      const first = App.matches[0];
+      let results = App.matches.every(match => match === first)
+      this.setState({ winner: results });
+    }
+  }
+
+  emptyArray() {
+    App.matches = [];
+  }
+
+  render() {
+    const { winner } = this.state;
+    const getLoser = () => {
+      return App.loser[Math.floor(Math.random() * App.loser.length)]
+    }
+    let repeatButton = null;
+    let winningSound = null;
+
+    if (winner !== null) {
+      repeatButton = <RepeatButton onClick={this.handleClick} />
+    }
+
+    if (winner) {
+      winningSound = <WinningSound />
+    }
+
+    return (
+      <div>
+        {winningSound}
+        <h1 style={{ color: 'white' }}>
+          <span>{winner === null ? 'Waiting…' : winner ? '🤑 Pure skill! 🤑' : getLoser()}</span>
+        </h1>
+
+        <div className={`spinner-container`}>
+          <Spinner onFinish={this.finishHandler} ref={(child) => { this._child1 = child; }} timer="1000" z="a" />
+          <Spinner onFinish={this.finishHandler} ref={(child) => { this._child2 = child; }} timer="1400" z="b" />
+          <Spinner onFinish={this.finishHandler} ref={(child) => { this._child3 = child; }} timer="2200" z="c" />
+          <div className="gradient-fade"></div>
+        </div>
+        {repeatButton}
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App
