0649f4735e64f45b6e57a849ca07f22cffd2c92c
ncy <nongchangyin@xd.com>
2023-07-03 22:52:00 +0800

    -

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {useState, useEffect} from 'react'
 
 import './SlotMachine.css'
 import './App.css'
+import bg from './bg.jpg'
 
 async function sleep(ms){
     return new Promise(r=>{
         setTimeout(r, ms)
     })
 }
+
 function Spinner({slot, speed}) {
     const unit = 56;
     let [init, setInit] = useState(false)
@@ -111,47 +113,53 @@ function App () {
     let [slot3, setSlot3] = useState(2)
     let finish = () => {}
     return (
-        <div className="text-white h-full flex flex-col jusitify-center items-center">
-            <div className="shadow-lg px-4 py-2 text-xl w-full flex flex-row justify-between items-center ">
-                <div >
-                    Token.ZkPenguin
-                </div>
-                <div>
-                    connect
-                </div>
-            </div>
-            <div className="p-4 flex flex-col gap-2">
-                <SlotMachine slot1={slot1} slot2={slot2} slot3={slot3}/>
-                <div className="slot-container px-2 py-1">
-                    <div className="flex flex-row justify-between items-center">
-                        <p>weger</p>
-                        <p>gain</p>
+        <div className="h-full w-full min-h-screen" syle={{backgroundImage:`url(${bg})`, backgroundPosition: '50% 50%'}} >
+            <div className=" min-h-screen text-white h-full flex flex-col jusitify-center items-center backdrop-blur-lg" syle={{backgroundColor: 'rgba(0, 0, 0, 0.9)'}}>
+                <div className="shadow-lg px-4 py-2 text-xl w-full flex flex-row justify-between items-center ">
+                    <div >
+                        Token.ZkPenguin
                     </div>
-                    <div className="flex flex-col gap-1">
-                        <SlotSample slots={[0, 1, 2]} gain="1000 $zkpn"/>
-                        <SlotSample slots={[0, 1, 2]}/>
-                        <SlotSample slots={[0, 1, 2]}/>
+                    <div>
+                        connect
                     </div>
                 </div>
-                <div className="flex flex-row justify-between w-full">
-                    <div onClick={ async e=> {
-                        if (slot1==null || slot2 == null ||  slot3 == null) 
-                            return
-                        setSlot1(null)
-                        setSlot2(null)
-                        setSlot3(null)
-                    }}> start</div>
-                    <div onClick={ async e=> {
-                        if (slot1!=null || slot2!=null || slot3!=null) 
-                            return
-                        setSlot1(null)
-                        await sleep(500)
-                        setSlot1(rs())
-                        await sleep(500)
-                        setSlot2(rs())
-                        await sleep(500)
-                        setSlot3(rs())
-                    }}> stop</div>
+                <div className="w-full grid grid-cols-2">
+                    <div className="w-full" >
+                    </div>
+                    <div className="p-4 flex flex-col gap-2">
+                        <SlotMachine slot1={slot1} slot2={slot2} slot3={slot3}/>
+                        <div className="slot-container px-2 py-1">
+                            <div className="flex flex-row justify-between items-center">
+                                <p>weger</p>
+                                <p>gain</p>
+                            </div>
+                            <div className="flex flex-col gap-1">
+                                <SlotSample slots={[0, 1, 2]} gain="1000 $zkpn"/>
+                                <SlotSample slots={[0, 1, 2]}/>
+                                <SlotSample slots={[0, 1, 2]}/>
+                            </div>
+                        </div>
+                        <div className="flex flex-row justify-between w-full">
+                            <div onClick={ async e=> {
+                                if (slot1==null || slot2 == null ||  slot3 == null) 
+                                    return
+                                setSlot1(null)
+                                setSlot2(null)
+                                setSlot3(null)
+                                }}> start</div>
+                            <div onClick={ async e=> {
+                                if (slot1!=null || slot2!=null || slot3!=null) 
+                                    return
+                                setSlot1(null)
+                                await sleep(500)
+                                setSlot1(rs())
+                                await sleep(500)
+                                setSlot2(rs())
+                                await sleep(500)
+                                setSlot3(rs())
+                                }}> stop</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
